d6c6f7e04b863e2927f8c8aed58412bc88c78ad0
Anton_Belyaev <a.b.work.ru@gmail.com>
2025-02-10 21:14:40 +0300

    task_4

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -24,32 +24,21 @@ class Student:
         if not self.grades.keys(): return 0
         all_summ = 0
         all_count = 0
-
         for k in self.grades.keys():
             all_summ = all_summ + sum(self.grades[k])
             all_count = all_count + len(self.grades[k])
-
         average_value = round(all_summ / all_count, 1)
         return average_value
-
     # */ Расчет среднего значения
+
     def __str__(self):
-        #if not self.grades:
-            #return (f'ОШИБКА: У Студента "{self.name}" нет отценок по курсу "{self.courses_in_progress}"\n'
-                    #f'---------------------------------------------')
-        #else:
-            #/* Список курсов в строке
         all_courses_in_progress=', '.join(self.courses_in_progress)
         all_courses_finished=', '.join(self.finished_courses)
-            #*/ Список курсов в строке
-
         return (f'Имя: {self.name}\n'
             f'Фамилия: {self.surname}\n'
             f'Средняя оценка за домашние задания: {self.Average_value()}\n'
             f'Курсы в процессе изучения: {all_courses_in_progress}\n'
-            f'Завершенные курсы: {all_courses_finished}\n'
-            f'---------------------------------------------')
-
+            f'Завершенные курсы: {all_courses_finished}\n')
 
 class Mentor:
     def __init__(self, name, surname):
@@ -95,6 +84,21 @@ class Reviewer(Mentor):
     def __str__(self):
         return  (f'Имя: {self.name}\n'
                 f'Фамилия: {self.surname}\n')
+
+
+def Mega_Average_value(people,course):
+    all_sum = 0
+    all_count = 0
+    for i in people:
+        if course in i.grades.keys():
+            all_sum = all_sum + sum(i.grades[course])
+            all_count = all_count  + len(i.grades[course])
+        else:
+            if all_sum==0:  return 0
+            else:   return all_sum / all_count
+    return all_sum/all_count
+
+
 #__Определяем СТУДЕНТОВ__________________________________________________________________________________
 
 Student1=Student('Alex','Won','Male')
@@ -106,28 +110,24 @@ Student1.finished_courses.append('Git - ясельки')
 Student2=Student('Kate','Honey','Female')
 Student2.courses_in_progress.append('Python')
 Student2.courses_in_progress.append('Java')
-Student2.courses_in_progress.append('Алгебра')
 Student2.finished_courses.append('Введение в програмирование')
 Student2.finished_courses.append('Общие понятия синтаксиса')
-
 Student3=Student('1','2','3')
 Student3.courses_in_progress.append('Java')
 Student3.courses_in_progress.append('Git')
 Student3.finished_courses.append('Введение в програмирование')
 Student3.finished_courses.append('Общие понятия синтаксиса')
-
 #==Определяем ЛЕКТОРОВ============================================================================
 Lector1=Lecturer('Martin','Hook')
 Lector1.courses_attached.append('Python')
 Lector1.courses_attached.append('Git')
-
 Lector2=Lecturer('Zak','Chan')
 Lector2.courses_attached.append('Java')
+Lector2.courses_attached.append('Python')
 #==Определяем РЕВЬЮЕРОВ============================================================================
 Reviewer1=Reviewer('Bob','Good')
 Reviewer1.courses_attached.append('Python')
 Reviewer1.courses_attached.append('Git')
-
 Reviewer2=Reviewer('Ron','Hool')
 Reviewer2.courses_attached.append('Java')
 #==============================================================================
@@ -141,15 +141,15 @@ Reviewer1.rate_student(Student1,'Python',5)     #Проверка - Значен
 Reviewer1.rate_student(Student1,'Python',10)    #Проверка - Значение отценки от 0 до 10
 Reviewer1.rate_student(Student1,'Python',11)    #Проверка - Значение отценки от 0 до 10
 Reviewer1.rate_student(Student1,'Python',1)     #Студент учится на курсе Ревьер преподает курс
-Reviewer1.rate_student(Student1,'Java',2)     #Студент НЕ учится на курсе Ревьер преподает курс
+Reviewer1.rate_student(Student1,'Java',2)       #Студент НЕ учится на курсе Ревьер преподает курс
 Reviewer2.rate_student(Student1,'Python',3)     #Студент учится на курсе Ревьер преподает курс
-Reviewer2.rate_student(Lector1,'Python',4)     #Ревьюер пытается установить отценку Лектору!!!
+Reviewer2.rate_student(Lector1,'Python',4)      #Ревьюер пытается установить отценку Лектору!!!
 
 print(Student1.grades)
 print('*/')
 
 print('#/*Студент выставляет отценку  - Летору / Проверка')
-Student1.rate_lector(Lector1,'Python',-1)   #Проверка - Значение отценки от 0 до 10
+Student1.rate_lector(Lector1,'Python',-1)         #Проверка - Значение отценки от 0 до 10
 Student1.rate_lector(Lector1,'Python',0)    #Проверка - Значение отценки от 0 до 10
 Student1.rate_lector(Lector1,'Python',1)    #Проверка - Значение отценки от 0 до 10
 Student1.rate_lector(Lector1,'Python',5)    #Проверка - Значение отценки от 0 до 10
@@ -158,22 +158,16 @@ Student1.rate_lector(Lector1,'Python',11)   #Проверка - Значение
 Student1.rate_lector(Lector1,'Python',1)    #Студент учится на курсе а Лектор преподает курс
 Student1.rate_lector(Lector2,'Python',2)    #Студент учится на курсе а Лектор НЕ преподает курс
 Student1.rate_lector(Lector2,'Java',3)      #Студент НЕ учится на курсе а Лектор преподает курс
-Student1.rate_lector(Reviewer1,'Git',4)    #Студент выставляет отценку Ревьеру!!!
-
+Student1.rate_lector(Reviewer1,'Git',4)     #Студент выставляет отценку Ревьеру!!!
 Reviewer1.rate_student(Student1,'Python',11)
 print(Lector1.grades)
 print('*/')
 
 
-Student1.rate_lector(Lector1,'Python',2)    #Студент учится на курсе а Лектор НЕ преподает курс
-Student1.rate_lector(Lector1,'Git',9)      #Студент НЕ учится на курсе а Лектор преподает курс
-Student2.rate_lector(Lector2,'Java',2)    #Студент учится на курсе а Лектор НЕ преподает курс
-Student2.rate_lector(Lector2,'Java',7)    #Студент учится на курсе а Лектор НЕ преподает курс
 
-#_/*Чистим словарь с отценками________________
+#_/*Чистим словарь с отценками перед - Полевми испытаниями________________
 Student1.grades.clear()
 Student2.grades.clear()
-Student3.grades.clear()
 Lector1.grades.clear()
 Lector2.grades.clear()
 #_*/
@@ -181,12 +175,15 @@ Lector2.grades.clear()
 #_/*Выставляем отценки Лекторам________________
 Student1.rate_lector(Lector1,'Python',2)
 Student1.rate_lector(Lector1,'Git',9)
+Student1.rate_lector(Lector2,'Python',9)
 Student2.rate_lector(Lector2,'Java',3)
 Student2.rate_lector(Lector2,'Java',4)
 #_*/________________
 #_/*Выставляем отценки Студентам________________
 Reviewer1.rate_student(Student1,'Python',5)
 Reviewer1.rate_student(Student1,'Python',10)
+Reviewer1.rate_student(Student2,'Python',7)
+Reviewer1.rate_student(Student2,'Python',8)
 Reviewer2.rate_student(Student2,'Java',3)
 Reviewer2.rate_student(Student2,'Java',4)
 #_*/________________
@@ -205,7 +202,6 @@ print(Lector2)
 print('')
 print('Student1 ->')
 print(Student1)
-print('')
 print('Student2 ->')
 print(Student2)
 print(f'\n ______________Сравнение Лекторов по средней отценке_____________________ ')
@@ -216,7 +212,16 @@ print(f"Lector1 ({Lector1.Average_value()}) < Lector2 "
 
 print(f'\n ______________Сравнение Студентов по средней отценке_____________________ ')
 print(f"Student1 ({Student1.Average_value()}) > Student2 "
-      f"({Student2.Average_value()}) = ", Student1.Average_value() > Student2.Average_value())
-print(f"Student1 ({Student1.Average_value()}) < Student2 "
-      f"({Student2.Average_value()}) = ", Student1.Average_value() < Student2.Average_value())
+      f"({Student2.Average_value()}) = ",
+      Student1.Average_value() > Student2.Average_value())
 
+print(f"Student1 ({Student1.Average_value()}) < Student2 "
+      f"({Student2.Average_value()}) = ",
+      Student1.Average_value() < Student2.Average_value())
+
+
+print(f'\nПодсчет средней оценки за домашние задания по всем СТУДЕНТам в рамках конкретного курса:')
+print(Mega_Average_value([Student1,Student2],'Python'))
+
+print(f'\nПодсчет средней оценки за лекции всех ЛЕКТОРов в рамках конкретного курса:')
+print(Mega_Average_value([Lector1,Lector2],'Python'))
